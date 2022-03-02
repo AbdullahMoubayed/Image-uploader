ae9ef59fcbf4ce1b99b7d5c57c312e5e67ccd566
Abdullah Moubayed <abdullah.moubayed@outlook.com>
2022-03-02 12:52:19 +0100

    0.7alpha

--- index.php
+++ index.php
@@ -13,6 +13,11 @@
 
 <section>
   <h1>Upload an image</h1>
+
+  <div class="result">
+    
+  </div>
+
   <form action="index.php" method="post" enctype="multipart/form-data">
     <input type="file" name="fileToUpload" id="fileToUpload">
   
@@ -43,33 +48,42 @@ require('php/thumbnail-generator.php');
 require('php/uploadImg.php');
 
  
- 
+$comment = "";
+$err = array("comment" => "", "image" => "", "file" => "");
+$state = array("db" => "", "file" => "", "thumb" => "");
+
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-
-  $comment = "";
-  $err = array("comment" => "", "image" => "", "other" => "");
-  $state = array("comment" => "", "email" => "", "pass" => "");
-  $reg = "/^[ a-z\d ]+$/i";
-
+  
+  
   // ----------------------------- Comment -----------------------------
-
+  
   if (empty($_GET['comment'])) {
     $err['comment'] = 'A comment is required';
   }
+  
+  function sanitize_comment($comment, $err) {
+    $reg = "/^[ a-z\d ]+$/i";
+    $ok = false;
 
-  if (!$err['comment']) {
-    
-    $comment = $_GET['comment'];
+    if (!$err['comment']) {
+      $comment = filter_var($comment, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+  
+      if(preg_match($reg, $comment) && mb_check_encoding($comment, 'UTF-8')){
+        $state["comment"] = "<b>$comment</b> is a valid comment";
+        $ok = true;
 
-    $comment = filter_var($comment, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+      } else {
+        $err["comment"] = "<b>$comment</b> is not a valid comment";
+      }
+    }
 
-    if(preg_match($reg, $comment) && mb_check_encoding($comment, 'UTF-8')){
-      $state["comment"] = "<b>$comment</b> is a valid comment";
-    } else {
-      $err["comment"] = "<b>$comment</b> is not a valid comment";
+    if($ok) {
+      return $comment;
     }
   }
+
+
 
   // ----------------------------- Image -----------------------------
  
@@ -83,53 +97,54 @@ if(isset($_POST["submit"])) {
  
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    // echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    $err["file"] =  "File is not an image.";
     $uploadOk = 0;
   }
  
  
   // Check if file already exists
   if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    $err["file"] =  "Sorry, file already exists.";
     $uploadOk = 0;
   }
   
   // Check file size
-  if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
+  if ($_FILES["fileToUpload"]["size"] > 2000000) {
+    $err["file"] =  "Sorry, your file is too large.";
     $uploadOk = 0;
   }
   
   // Allow certain file formats
   if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
   && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $err["file"] =  "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
   }
   
   // Check if $uploadOk is set to 0 by an error
   if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    $err["image"] =  "Sorry, your file was not uploaded.";
   // if everything is ok, try to upload file
   } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      $comment = sanitize_comment($_POST["comment"], $err);
 
       $image_name = $_FILES["fileToUpload"]["name"];
       $thumb_name = "thumb-".$_FILES["fileToUpload"]["name"];
 
-      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+      $state["file"] = "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
 
       createThumbnail(100, 100, $target_file, "uploads/". $thumb_name);
-      echo "Thumbnail uploaded successfully.";
+      $state["thumb"] = "Thumbnail uploaded successfully.";
 
-      uploadImg($image_name, $thumb_name, $comment);
-      echo "Information saved successfully to database.";
+      uploadImg($image_name, $thumb_name, $_POST["comment"]);
+      $state["db"] = "Information saved successfully to database.";
 
     } else {
-      echo "Sorry, there was an error uploading your file.";
+      $err["file"] = "Sorry, there was an error uploading your file.";
     }
   }
 }
